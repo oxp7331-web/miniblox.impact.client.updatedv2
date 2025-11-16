959c282bee35ba32aa0bea96f4513bf3672b0fcc
oxp7331-web <oxp7331@gmail.com>
2025-11-16 08:44:04 +0300

    Update tampermonkey.user.js
    Signed-off-by: oxp7331-web <oxp7331@gmail.com>

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -6,61 +6,16 @@
 // @author       7GrandDad, ProgMEM-CC, TheM1ddleM1n, he557, RealPacket/DataM0del (Codeberg/Github names), BlueDragon7327, whoppps, annepaurush
 // @match        https://miniblox.io/
 // @match        https://miniblox.org/
-// @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
+// @icon         https://raw.githubusercontent.com/oxp7331-web/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        unsafeWindow
-// @require      https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/vav4inject.js
+// @require      https://raw.githubusercontent.com/oxp7331-web/miniblox.impact.client.updatedv2/refs/heads/main/vav4inject.js
 // @run-at       document-start
-// ** This is Important For Updating **
-// @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
-// @downloadURL  https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @updateURL    https://raw.githubusercontent.com/oxp7331-web/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/oxp7331-web/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @license      AGPL-3.0-only
-// 
-// =========================================== EULA ==========================================================
-//  Before using, read https://github.com/ProgMEM-CC/miniblox.impact.client.updatedv2/blob/main/EULA.md
 //
-// ============================================== The Developer Team: for Impact For Miniblox! ===============================================
-//  ProgMEM-CC (github.com/ProgMEM-CC)
-//  TheM1ddleM1n (github.com/TheM1ddleM1n)
-//  DataM0del (github.com/datam0del)
-//  BlueDragon7327 (github.com/BlueDragon7327) 
-//  dtkiller-jp (github.com/dtkiller-jp)
-//  whoppps (github.com/whoppps)
-//  annepaurush (github.com/annepaurush)
-//
-//  i dont think we need anymore devs lol :trollface: (unless required)
-//  
-// =============================================== Credits To ================================================
-//    7GrandDad, the OG Creator of the Vape V4 hacked client for Miniblox!
-//    he557, doing absolutely nothing other than forking the already working Vape for Miniblox fork!
-//    ProgMEM-CC, the upgrader and reviver of the client as Impact V4 for Miniblox!
-//    TheM1ddleM1n, the one who never fails to amaze, creating a LB themed ClickGUI and creating new modules!
-//    RealPacket, (codeberg: https://codeberg.org/RealPacket) for the inspiration of reviving the hacks and making a genius desync for Fly and Nofall, also making .friend command, Antiblind module etc!
-//    BlueDragon7327, Module Researcher and solver!
-//    dtkiller-jp, Bug reporter and solver!
-//    whoppps, issue raiser
-//
-// ==================================== Extra Credits To! =====================================================
-//    The js.org team, who kindly provided us with the subdomain impactminiblox.js.org for the project site
-//    Vercel.app, which allowed me to have an alternative subdomain at minibloxhacks.vercel.app
-//    Netlify.app, which also allowed me to have an alternative subdomain at minibloxhacks.netlify.app
-//
-// ============================================ LICENSE ======================================================
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License version 3
-// as published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>,
-// or see the project page of Impact V4 for miniblox.
-//
-// For Additional Terms:
-// Please refer to EULA.md
-//
+
+
 // ==/UserScript==
